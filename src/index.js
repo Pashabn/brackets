a52eace5e830515086f150195c62195b17539724
github-username <72346436+Pashabn@users.noreply.github.com>
2022-07-15 00:20:54 +0300

    feat: added function's body

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    const check = (x, y = bracketsConfig.map(i => i.join('')).filter(i => x.includes(i))) => {
+        if (!y.length) return false
+        for (let i = 0; i < y.length; i++) {
+            x = x.replace(y[i], '')
+        }
+        return !x ? true : check(x)
+    }
+    return check(str)
 }
+
